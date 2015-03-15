36cc59c5b96210205df3cbb8902106b457978e51
Paul Ramnora <paul@ramnora.com>
2015-03-15 21:43:42 +0000

    Create pageA1.bat
    Series of inter-linked pages continued...

---
+++ pageA1.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+ECHO MAIN TITLE TEXT
+ECHO.
+ECHO Table Of Contents:-
+ECHO.
+ECHO 1: SUB TITLE TEXT ONE
+ECHO.
+ECHO CONTENT GOES HERE
+ECHO.
+PAUSE
+REM *** load the next page...
+pageA2.bat
