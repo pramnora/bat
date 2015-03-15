f0efddb8241f82c4e17b9d8b69d903ae947538e8
Paul Ramnora <paul@ramnora.com>
2015-03-15 17:56:07 +0000

    Create singlpag.bat
    Creates a single page of text message to be read.

---
+++ singlpag.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+ECHO TITLE goes here
+ECHO.
+ECHO Line 1: Messsage text goes here.
+ECHO Line 2: Messsage text goes here.
+ECHO Line 3: Messsage text goes here.
+ECHO Line 4: Messsage text goes here.
+ECHO Line 5: Messsage text goes here.
+ECHO Line 6: Messsage text goes here.
+ECHO.
+pause
