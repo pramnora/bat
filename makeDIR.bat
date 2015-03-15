f832379b23e3e6cec7cb9690041c3b44e5d43898
Paul Ramnora <paul@ramnora.com>
2015-03-15 18:24:43 +0000

    Create makeDIR.bat
    How to create folder DIRectories using the MKDIR/shorthand: MD command

---
+++ makeDIR.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+REM *** You could also use MKDIR command...to make a 'new' DIRectory folder...;
+REM --- I prefer using the shorthand: MD, instead.
+MD test
+REM *** Next, use the DIR statement to list the contents inside of the current DIRectory
+REM ...including all visible files/folders; in order to check if the folder actually was created or not...? 
+DIR
