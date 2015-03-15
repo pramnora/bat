b6695ad8bb430b57fda6804db16228264c342d93
Paul Ramnora <paul@ramnora.com>
2015-03-15 18:44:54 +0000

    Update loadFils.bat
    Added some further descriptive REM comments text...

--- loadFils.bat
+++ loadFils.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
-REM *** Simply just type in the filename + dot + filename extension
+REM *** Files ending in extension name: .COM, .BAT, .EXE are all automatically executable files...;
+REM *** in order to make them load; then, just simply type in the filename + dot + filename extension...
 REM *** (If necessary, then, precede the filename+dot+filename extension by the full pathname, as well.)
+REM *** Below, I will attempt to automatically load in 3 files each of which is native to Windows platforms...
 notepad.exe
 wordpad.exe
 calc.exe
