5d51ec21f78f5e31499021d06e83f23a4ac0a13d
Paul Ramnora <paul@ramnora.com>
2015-03-15 18:49:54 +0000

    Update loadFils.bat
    Changed loading [wordpad.exe] to become [write.exe], instead; apparently wordpad goes by a different name.

--- loadFils.bat
+++ loadFils.bat
@@ -4,5 +4,5 @@ REM *** in order to make them load; then, just simply type in the filename + dot
 REM *** (If necessary, then, precede the filename+dot+filename extension by the full pathname, as well.)
 REM *** Below, I will attempt to automatically load in 3 files each of which is native to Windows platforms...
 notepad.exe
-wordpad.exe
+write.exe
 calc.exe
