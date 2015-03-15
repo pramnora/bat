2cf92361f2f11004bc3873af83488add6647d585
Paul Ramnora <paul@ramnora.com>
2015-03-15 23:58:29 +0000

    Update forloop1.bat
    Added source code commentary note as to how many % sign symbols to use with varName depending on context: inside of batch file use 2(%%varName)/inside of DOS window use 1(%varName).

--- forloop1.bat
+++ forloop1.bat
@@ -1,3 +1,5 @@
 @ECHO OFF
+REM * NOTE: Inside of the batch file use %%VarName/(double percentage symbols);
+REM         inside of the DOS Command prompt window use: %VarName/(single percentage symbol). 
 FOR %%x IN (1 2 3) DO ECHO %%x
 PAUSE
