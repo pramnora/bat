07cfeb9d844ce9121ebf14d28c0ee54733446d55
Paul Ramnora <paul@ramnora.com>
2015-03-15 18:39:42 +0000

    Create loadFils.bat
    How to load files, automatically, using a single .batch file program.

---
+++ loadFils.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+REM *** Simply just type in the filename + dot + filename extension
+REM *** (If necessary, then, precede the filename+dot+filename extension by the full pathname, as well.)
+notepad.exe
+wordpad.exe
+calc.exe
